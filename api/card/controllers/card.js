8d9b4f60ece6a571d1e00e7728fc4980d66ec1c8
Mahmood Heidari <maktoobgar@gmail.com>
2020-03-28 11:41:57 +0430

    Random cards and projects

--- api/card/controllers/card.js
+++ api/card/controllers/card.js
@@ -5,4 +5,24 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports =
+{
+  random: async (ctx) => {
+    try
+    {
+      var rand1 = Math.floor(Math.random() * 92);
+      var rand2 = Math.floor(Math.random() * 92);
+      while(rand1 === rand2 || rand1 === 0 || rand2 === 0)
+      {
+        rand1 = Math.floor(Math.random() * 92);
+        rand2 = Math.floor(Math.random() * 92);
+      }
+      const result = await strapi.query('card').find({ id_in: [rand1, rand2]});
+      ctx.send(result);
+    }
+    catch (e)
+    {
+      ctx.send(e.message);
+    }
+  }
+};
--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -5,4 +5,20 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports =
+{
+  random: async (ctx) => {
+    //36
+    try
+    {
+      var rand = Math.floor(Math.random() * 36);
+      while(rand === 0) {rand = Math.floor(Math.floor(Math.random() * 36));}
+      const result = await strapi.query('project').find({id: rand});
+      ctx.send(result);
+    }
+    catch (e)
+    {
+      ctx.send(e.message);
+    }
+  }
+};
