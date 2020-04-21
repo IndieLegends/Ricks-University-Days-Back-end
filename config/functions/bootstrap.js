878732a4750466f576e1c4a316249731d24797b2
ali <marefati110@gmail.com>
2020-04-21 23:11:56 +0430

    bugfix

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,13 +1,7 @@
-'use strict';
+"use strict";
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- *
- * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
- */
-
-module.exports = () => {};
+module.exports = () => {
+    // import socket io
+    var io = require("socket.io")(strapi.server);
+    strapi.io = io;
+};
